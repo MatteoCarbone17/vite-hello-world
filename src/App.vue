50c16c8c89a2ec54e509bae603dbe57389a8ba43
MatteoCarbone17 <matteocarbone17@live.com>
2022-12-23 17:24:55 +0100

    use tag p and lorem to test

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,22 @@
 <script>
 import AppTitle from './components/AppTitle.vue';
+import MainComponent from './components/Maincomponent.vue';
 
 export default {
   components: {
-    AppTitle
+    AppTitle,
+    MainComponent,
+    
   }
 }
 
 </script>
 
 <template>
+  
   <AppTitle />
+  <MainComponent />
+
 </template>
 
 <style scoped>
---
+++ src/components/MainComponent.vue
@@ -0,0 +1,20 @@
+<script>
+
+</script>
+
+<template>
+  <main>
+    <img src="https://picsum.photos/seed/picsum/200/300" alt="" srcset="">
+    <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem dolore, assumenda id iusto nemo culpa natus. Repudiandae, libero officia, atque eos, blanditiis delectus totam quis facere placeat ratione quae porro.
+    </p>
+
+  </main>
+</template>
+
+<style scoped>
+p{
+    color: brown;
+    font-size: 1.5rem;
+}
+</style>
